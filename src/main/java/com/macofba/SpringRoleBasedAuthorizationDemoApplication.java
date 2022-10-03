2b27cd634e531ac2412c8ce480b059e10cfa7446
ishopjapan <thoitran2107@gmail.com>
2022-10-03 15:25:46 +0700

    bai tap thuc hanh phan quyen

---
+++ src/main/java/com/macofba/SpringRoleBasedAuthorizationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.macofba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoleBasedAuthorizationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRoleBasedAuthorizationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macofba/SpringRoleBasedAuthorizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macofba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoleBasedAuthorizationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
